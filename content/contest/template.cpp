5e68554807b8d25bce33d78a348bc859e83606fb
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2024-12-08 19:21:19 +0530

    make it bits

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define int long long
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
@@ -8,7 +9,14 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-int main() {
+void solve() {
+
+}
+
+signed main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
+	int t; cin >> t; while (t--) {
+		solve();
+	}
 }
